e460dc4bed5fb24198f33fcac144b52ceb9b9ca0
weidermartenn <weidermartenn@gmail.com>
2024-12-09 20:51:27 +0300

    hjkklk

--- f14/src/widgets/Header/ui/Header.vue
+++ f14/src/widgets/Header/ui/Header.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="block px-4 py-2">
-        <img src="/wflogo.svg" alt="logo" class="w-16 h-16 bg-red-500">
+    <div class="flex flex-row items-center justify-center px-8 py-2">
+        <Transition name="translate" mode="out-in" appear>
+            <img
+            :src=logo alt="logo" class="w-16 h-16 bg-blue-800 shadow-xl shadow-blue-900">
+        </Transition>
         
     </div>
 </template>
 
 <script setup lang="ts">
-
+import logo from "@/app/assets/wflogo.svg"
 </script>
 
 <style scoped>
+.translate-enter-active, 
+.translate-leave-active {
+    transition: all 0.5s ease;
+}
 
+.translate-enter-from,
+.translate-leave-to {
+    opacity: 0;
+    transform: translateX(-100px);
+}
 </style>
--- f14/tailwind.config.js
+++ f14/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         "IMB": ["IBM Plex Sans", "sans-serif"],
       },
       colors:{
-        "backgroundDark": "#0E0E0E",
+        "backgroundDark": "#1E1E1E",
         "backgroundLight": "#F1EAE1",
         "onBackgroundDark": "#0E0B0B",
         "onBackgroundLight": "#F1EAE1",
