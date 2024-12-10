08a8382f542d67ec7f5d4a7826c606ca523ea173
weidermartenn <weidermartenn@gmail.com>
2024-12-10 21:17:57 +0300

    asdasd

--- f14/src/views/Main/ui/MainPage.vue
+++ f14/src/views/Main/ui/MainPage.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-
+    <div class="flex flex-col p-6 gap-4">
+        <div class="space-y-6">
+            <span class="text-2xl">Новости</span>
+            <div class="flex gap-6">
+                <a href="#" v-for="i in 3" 
+                    class="w-96 h-52 bg-onBackgroundDark rounded-lg hover:scale-105 hover:shadow-sm hover:shadow-blue-800 duration-150"
+                >
+                    
+                </a>
+            </div>
+        </div>
+        <div class="space-y-6">
+            <span class="text-2xl">Погода</span>
+        </div>
+        <div class="space-y-6">
+            <span class="text-2xl">Курс валют</span>
+        </div>
     </div>
 </template>
 
--- f14/src/widgets/Header/ui/Header.vue
+++ f14/src/widgets/Header/ui/Header.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="flex flex-row items-center justify-center px-8 py-2">
-        <Transition name="translate" mode="out-in" appear>
+    <div class="flex flex-row items-center justify-center px-8 py-2 space-x-96">
+        <TransitionGroup name="translate" mode="out-in">
             <img
             :src=logo alt="logo" class="w-16 h-16 bg-blue-800 shadow-xl shadow-blue-900">
-        </Transition>
+            <section class="flex flex-row items-center space-x-6 font-medium">
+                <i class="fa-solid fa-circle-user text-3xl"></i>
+                <a href="#" class="hover:text-blue-500 duration-300">Войти</a>
+                <p href="#" class="hover:text-blue-500 duration-300 cursor-default">|</p>
+                <a href="#" class="hover:text-blue-500 duration-300">Зарегистрироваться</a>
+            </section>
+        </TransitionGroup>
         
     </div>
 </template>
@@ -21,6 +27,6 @@ import logo from "@/app/assets/wflogo.svg"
 .translate-enter-from,
 .translate-leave-to {
     opacity: 0;
-    transform: translateX(-100px);
+    transform: translateY(-100px);
 }
 </style>
